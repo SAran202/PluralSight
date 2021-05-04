121e2fd20532c27dad2186733f6721bda1286318
SAran Periyasamy <srisaran246@gmail.com>
2021-05-04 20:23:21 +0530

    May-4 Work
    1.Dapper Basics
    2.Dapper Beyond the Basics
    3.Setting up an MVC Application
    4.Working with Real Data Using Entity Framework Core

---
+++ May-4/BethanysPieShop/BethanysPieShop/Models/Pie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class Pie
+    {
+        public int PieId { get; set; }
+        public int Name { get; set; }
+        public int Short { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+        public int PieId { get; set; }
+
+
+    }
+}
