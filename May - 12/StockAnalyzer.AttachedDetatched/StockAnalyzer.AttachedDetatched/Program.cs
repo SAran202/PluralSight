411965fe60662ed0d89028a3a42f3b5bd1dd4e77
SAran Periyasamy <srisaran246@gmail.com>
2021-05-12 20:26:09 +0530

    May-12 Works
    1.Async and Await Advanced Topics and Best Practices
    2.Asynchronous Programming Deep Dive
    --------------------------------------------
    3.Clean Coding Principles in C#

---
+++ May - 12/StockAnalyzer.AttachedDetatched/StockAnalyzer.AttachedDetatched/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockAnalyzer.AttachedDetatched
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var task = Task.Factory.StartNew(async() => {
+                await Task.Delay(2000);
+                return "Plural";
+            }).Unwrap();
+
+            var result = await task;
+
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+        }
+    }
+}
