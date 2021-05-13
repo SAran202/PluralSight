fb0fbf4966f6561bd60da7f500c641f47ce8be82
SAran Periyasamy <srisaran246@gmail.com>
2021-05-14 00:55:56 +0530

    May-13 Works
    1.Adding Background Tasks to ASP.NET Core Applications
    2.Building .NET Core Worker Services
    3.Advanced Hosted Service Concepts
    4.Running Worker Services in Production

---
+++ May- 13/BackGroundTask.Demo/BackGroundTask.Demo/BackGroundPrinter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackGroundTask.Demo
+{
+    public class BackGroundPrinter : IHostedService, IDisposable
+    {
+        private readonly ILogger<BackGroundPrinter> logger;
+        private int number = 0;
+        private Timer timer;
+
+        public BackGroundPrinter(ILogger<BackGroundPrinter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            timer = new Timer(o =>
+            {
+                Interlocked.Increment(ref number);
+                logger.LogInformation("Printing from worker");
+            },
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Printing worker Stopping");
+            return Task.CompletedTask;
+        }
+    }
+}
