d6de1a230d251a583cd01dcb0e8baeddc5d1bbb8
SAran Periyasamy <srisaran246@gmail.com>
2021-05-19 19:51:52 +0530

    May-19 Works
    Testing

---
+++ May-19/InventoryService/InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,58 @@
+﻿using InventoryService.Models;
+using InventoryService.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryService.Controllers
+{
+    [Route("v1/")]
+    [ApiController]
+    public class InventoryController : Controller
+    {
+        private readonly IInventoryServices _services;
+
+        public InventoryController(IInventoryServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        [Route("AddInventoryItems")]
+        public ActionResult<InventoryItems> AddInventoryItems(InventoryItems items)
+        {
+            var inventoryItems = _services.AddInventoryItems(items);
+            if(inventoryItems == null)
+            {
+                return NotFound();
+            }
+            return inventoryItems;
+        }
+        [HttpGet]
+        [Route("GetInventoryItems")]
+        public ActionResult<Dictionary<string, InventoryItems>>GetInventoryItems()
+        {
+            var inventoryItems = _services.GetInventoryItems();
+
+            if(inventoryItems.Count == 0)
+            {
+                return NotFound();
+            }
+            return inventoryItems;
+        }
+
+        [HttpDelete]
+        [Route("DelInventoryItems")]
+        public ActionResult<InventoryItems> RemoveInventoryItems(InventoryItems items)
+        {
+            var inventoryItems = _services.RemoveInventoryItems(items);
+            if (inventoryItems == null)
+            {
+                return NotFound();
+            }
+            return inventoryItems;
+        }
+    }
+}
---
+++ May-19/InventoryService/InventoryService/Services/InventoryServices.cs
@@ -0,0 +1,31 @@
+﻿using InventoryService.Models;
+using System.Collections.Generic;
+
+namespace InventoryService.Services
+{
+    public class InventoryServices : IInventoryServices
+    {
+        private readonly Dictionary<string, InventoryItems> _inventoryItems;
+
+        public InventoryServices()
+        {
+            _inventoryItems = new Dictionary<string, InventoryItems>();
+        }
+
+        public InventoryItems AddInventoryItems(InventoryItems items)
+        {
+            _inventoryItems.Add(items.ItemName, items);
+            return items;
+        }
+
+        public Dictionary<string, InventoryItems> GetInventoryItems()
+        {
+            return _inventoryItems;
+        }
+
+        public InventoryItems RemoveInventoryItems(InventoryItems items)
+        { 
+            return items;
+        }
+    }
+}
