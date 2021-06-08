db9ac5f1107699480c9a8d8234adba56d8507837
SAran Periyasamy <srisaran246@gmail.com>
2021-06-08 20:18:51 +0530

    June-8
    Creating an S3 Bucket from Asp.net Core
    Lambda Project

---
+++ June-8/SeBucketTst/SeBucketTst/Controllers/SeBucketController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeBucketTst.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeBucketTst.Controllers
+{
+    [Produces("ApplicationException/Json")]
+    [Route("Api/S3Bucket")]
+    public class SeBucketController : Controller
+    {
+        private readonly IS3Service _service;
+        public SeBucketController(IS3Service service)
+        {
+            _service = service;
+        }
+        [HttpPost("{bucketName}")]
+        public async Task<IActionResult> CreateBucket([FromRoute] string bucketName)
+        {
+            var responce = await _service.CreateBucketAsync(bucketName);
+            return Ok();
+        }
+    }
+}
---
+++ June-8/SeBucketTst/SeBucketTst/Services/IS3Service.cs
@@ -0,0 +1,10 @@
+﻿using SeBucketTst.Models;
+using System.Threading.Tasks;
+
+namespace SeBucketTst.Services
+{
+    public interface IS3Service
+    {
+        Task<S3Response> CreateBucketAsync(string bucketName);
+    }
+}
